c8f12094968381c75ac8e8ee3f46fff74293c39b
jarekm80 <jarekm80>
2024-09-18 13:38:25 +0200

    plotki

---
+++ .ipynb_checkpoints/cw01-checkpoint.py
@@ -0,0 +1,9 @@
+imie:str = "jarek"
+nazwisko:str = "bulbas"
+print(imie.capitalize() + " " + nazwisko.capitalize())
+
+print("koniec")
+#help(dir(__builtins__))  
+
+napis = "ala ma kota"
+print(napis.index("a"))
---
+++ .ipynb_checkpoints/funkcje-checkpoint.py
@@ -0,0 +1,12 @@
+operacja_in = input("Podaj operacje: ")
+a = input("Podaj a ")
+b = input("Podaj b ")
+
+operacja = {
+    "+": "add",
+    "-": "sub",
+}
+
+def add(a,b)
+
+def funkcja(nazwa):
